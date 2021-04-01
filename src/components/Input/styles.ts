d485cf5c809d9856646cbc74d5d0bfe081562905
marcos <marcosrodriguesbrugnaro@gmail.com>
2021-03-31 22:43:09 -0300

    Finished signin page.

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  background: #232129;
+  border: 2px solid #232129;
+  border-radius: 10px;
+  color: #666360;
+  display: flex;
+  padding: 16px;
+  width: 100%;
+  & + div {
+    margin-top: 8px;
+  }
+  input {
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+    flex: 1;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+  svg {
+    margin-right: 16px;
+  }
+`;
