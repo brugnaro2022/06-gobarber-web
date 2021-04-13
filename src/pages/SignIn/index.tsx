7be2c7bdd592cdbebc3874b4480b88d1267019f9
marcos <marcosrodriguesbrugnaro@gmail.com>
2021-04-13 20:44:19 -0300

    SignUp page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
-import logoImg from '../assets/logo.svg';
+import logoImg from '../../assets/logo.svg';
 
-import Input from '../components/Input';
-import Button from '../components/Button';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Background, Container, Content } from './styles';
 
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import signInBackgroundImg from '../assets/sign-in-background.png';
+import signUpBackgroundImg from '../../assets/sign-up-background.png';
 
 export const Background = styled.div`
-  background: url(${signInBackgroundImg}) no-repeat center;
+  background: url(${signUpBackgroundImg}) no-repeat center;
   background-size: cover;
   flex: 1;
 `;
@@ -43,7 +43,7 @@ export const Content = styled.div`
 
   > a {
     align-items: center;
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     display: flex;
     margin-top: 24px;
@@ -53,7 +53,7 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#f4ede8')};
     }
   }
 `;
